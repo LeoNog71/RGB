f55e321b68be92f210cb2afdc4aa6515ac49e5f5
nogueira <leosil.2015@alunos.utfpr.edu.br>
2018-04-23 11:32:10 -0300

    1 commit

--- RGB/src/rgb/RGB.java
+++ RGB/src/rgb/RGB.java
@@ -5,6 +5,14 @@
  */
 package rgb;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author nogueira
@@ -15,7 +23,30 @@ public class RGB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            BufferedImage imagem = ImageIO.read(new File("/home/nogueira/Downloads/indice.jpeg"));
+            
+            int altura = imagem.getHeight();
+            int largura = imagem.getWidth();
+            int [] red = new int[256];
+            int [] green = new int[256];
+            int [] blue = new int[256];
+            
+            for(int i = 0;i < altura;i++){
+                for(int j = 0;j<largura;j++){
+                    red[new Color(imagem.getRGB(j, i)).getRed()]++;
+                    green[new Color(imagem.getRGB(j, i)).getGreen()]++;
+                    blue[new Color(imagem.getRGB(j, i)).getBlue()]++;  
+                }
+            }
+            
+            
+            
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(RGB.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
