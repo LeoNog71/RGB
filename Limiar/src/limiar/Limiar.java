3f66ef86971fe25823ce94150834424bf6a8ce66
nogueira <leosil.2015@alunos.utfpr.edu.br>
2018-05-09 18:32:20 -0300

    Leonardo 17:45

---
+++ Limiar/src/limiar/Limiar.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package limiar;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author nogueira
+ */
+public class Limiar {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+         try {
+            String pasta= "";
+            JFileChooser jfc=new JFileChooser();
+            
+            jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            
+            if(jfc.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+                pasta= jfc.getSelectedFile().getAbsoluteFile().toString();            
+                System.out.println(pasta);
+            }
+            
+            
+            
+            BufferedImage imagem = ImageIO.read(new File(pasta));
+            
+            int altura = imagem.getHeight();
+            int largura = imagem.getWidth();
+            Double media= 0.0;
+            Boolean[][] mascaraBoleean = new Boolean[altura][largura];
+            int tomDeCinza;
+            int pixels[] = new int[altura * largura];
+            WritableRaster r = imagem.getRaster();
+            
+            for(int i = 0;i < altura;i++){
+                for(int j = 0;j<largura;j++){
+                    
+                    r.getPixel(j, i, pixels);
+                    
+                    media +=new Color(imagem.getRGB(j, i)).getRed();
+                    media +=new Color(imagem.getRGB(j, i)).getGreen();
+                    media +=new Color(imagem.getRGB(j, i)).getBlue();
+                    media = media/3;
+            
+                    
+                    tomDeCinza = media.intValue();
+                    pixels[0] = tomDeCinza;
+                    pixels[1] = tomDeCinza;
+                    pixels[2] = tomDeCinza;
+                    // escrevo no WritableRaster novamente a matriz de pixels desse eixo...
+                    r.setPixel(j, i, pixels);
+                    
+                    
+                    //System.out.println(tomDeCinza);
+                    mascaraBoleean[i][j] = tomDeCinza > 128;
+                }
+                media = 0.0;
+            }
+        imagem.setData(r);
+        String figuraModi= "nova figura";
+        try {
+              ImageIO.write(imagem, "png", new File(figuraModi+".png"));
+        } catch (IOException ex) {
+               System.out.println("Erro 2.:"+ex);
+        } catch (Exception ex) {
+               System.out.println("Erro 2.1.:"+ex);
+        }
+         } catch (IOException ex) {
+             System.out.println(ex);
+        }
+    }
+    
+}
